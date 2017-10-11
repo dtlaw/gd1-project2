250944862dc4d51200e1dbc81fb3d041aa6cdadd
myoder345 <myoder345@gmail.com>
2017-10-11 17:44:26 -0400

    Firing now works in mode 2, bullets are killed after leaving the screen. Final commit of class.

--- phaser_project/swipeMove.js
+++ phaser_project/swipeMove.js
@@ -15,15 +15,18 @@ swipeMoveState.prototype.create = function() {
         align: "center"
     }
 
+    this.attacks = game.add.group();
+    this.attacks.enableBody = true;
+
     game.add.text(game.world.centerX - 250, 48, "Swipe up or down to change lanes", style);
     game.add.text(game.world.centerX - 150, 96, "Tap to fire an attack", style);
 
     this.player = game.add.sprite( game.world.width - 200, game.world.height / 2, "player");
 
-    // Reevaluate Scale after we get actual assets
+    // Reevaluate Scale after we get actual assets, this is just for the placeholders
     this.player.scale.setTo(0.35, 0.35);
 
-
+    game.input.onDown.add(this.musicBlast, this);
 }
 
 swipeMoveState.prototype.update = function() {
@@ -32,4 +35,11 @@ swipeMoveState.prototype.update = function() {
 
 swipeMoveState.prototype.musicBlast = function() {
     console.log("he attac");
+    let attack = this.attacks.create(this.player.x, this.player.y, "attack");
+    attack.scale.setTo(0.35, 0.35);
+    attack.body.velocity.x = -200;
+
+    // Kill offscreen objectss
+    attack.checkWorldBounds = true;
+    attack.outOfBoundsKill = true;
 }

76fe097f00601ae2d4a531439fa2716738df8d07
myoder345 <myoder345@gmail.com>
2017-10-15 14:27:18 -0400

    Added player movement behavior. Still in testing, committing for phone testing purposes.

--- phaser_project/gameplay.js
+++ phaser_project/gameplay.js
@@ -2,7 +2,7 @@ let gamePlayState = function() {
 
 }
 
-// const swipeDistance = 200;
+let lanes = new Array(4);
 
 gamePlayState.prototype.gameplay = function() {
     // NOTHING GOES HERE
@@ -15,26 +15,52 @@ gamePlayState.prototype.create = function() {
         align: "center"
     }
 
+    lanes[0] = new Phaser.Point(game.world.width - 200, game.world.height - 550);
+    lanes[1] = new Phaser.Point(game.world.width - 200, game.world.height - 400);
+    lanes[2] = new Phaser.Point(game.world.width - 200, game.world.height - 250);
+    lanes[3] = new Phaser.Point(game.world.width - 200, game.world.height - 100);
+
     this.attacks = game.add.group();
     this.attacks.enableBody = true;
 
     game.add.text(game.world.centerX - 250, 48, "Swipe up or down to change lanes", style);
     game.add.text(game.world.centerX - 150, 96, "Tap to fire an attack", style);
 
-    this.player = game.add.sprite( game.world.width - 200, game.world.height / 2, "player");
+    this.player = game.add.sprite( lanes[2].x, lanes[2].y, "player");
+    this.player.lane = 2;
 
     // Reevaluate Scale after we get actual assets, this is just for the placeholders
     this.player.scale.setTo(0.35, 0.35);
 
-    game.input.onDown.add(this.musicBlast, this);
+    game.input.onUp.add(this.inputCheck, this);
 }
 
 gamePlayState.prototype.update = function() {
 
 }
 
+gamePlayState.prototype.inputCheck = function() {
+    // Swipe upward
+    if(game.input.activePointer.positionUp.y - game.input.activePointer.positionDown.y <= -1 * swipeDistance
+        && this.player.lane > 0) {
+        --this.player.lane
+        this.player.x = lanes[this.player.lane].x;
+        this.player.y = lanes[this.player.lane].y;
+    }
+    // Swipe downward
+    else if(game.input.activePointer.positionUp.y - game.input.activePointer.positionDown.y >= swipeDistance
+        && this.player.lane < 3) {
+        ++this.player.lane
+        this.player.x = lanes[this.player.lane].x;
+        this.player.y = lanes[this.player.lane].y;
+    }
+    // Fire
+    else {
+        this.musicBlast();
+    }
+}
+
 gamePlayState.prototype.musicBlast = function() {
-    console.log("he attac");
     let attack = this.attacks.create(this.player.x, this.player.y, "attack");
     attack.scale.setTo(0.35, 0.35);
     attack.body.velocity.x = -200;

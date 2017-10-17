263942af4cb4b6c0437ddf2a138d9d6374ec8bf1
ErykaG <egreaves3@gmail.com>
2017-10-17 19:07:01 -0400

    Enemies appear in each lane. Accidentally was working on master. Switching to ErykaBranch now..

--- phaser_project/gameplay.js
+++ phaser_project/gameplay.js
@@ -30,6 +30,7 @@ gamePlayState.prototype.create = function() {
     game.add.text(game.world.centerX - 250, 48, "Swipe up or down to change lanes", style);
     game.add.text(game.world.centerX - 150, 96, "Tap to fire an attack", style);
 
+    // PLAYER
     this.player = game.add.sprite( lanes[2].x, lanes[2].y, "player");
     this.player.lane = 1;
     this.player.animations.add("idle", [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
@@ -38,14 +39,27 @@ gamePlayState.prototype.create = function() {
 
     // Reevaluate Scale after we get actual assets, this is just for the placeholders
     // this.player.scale.setTo(1, 1);
-
     game.input.onUp.add(this.inputCheck, this);
 
     this.player.animations.play("idle");
 
+    // ENEMIES
+    this.enemies = game.add.group();
+    this.enemies.enableBody = true;
+    // No gravity because arial view
+    for (let i = 0; i < 4; i++) {
+        // Eventually will be random in-lane spawn
+        let enemy = this.enemies.create(0, lanes[i].y, "bEnemy");
+        enemy.body.gravity.y = 0;
+        enemy.body.velocity.x = 80;
+    }
+    //this.enemies.animations.add("move", [0], 10, true);
+
 }
 
 gamePlayState.prototype.update = function() {
+    game.physics.arcade.collide(this.enemies, this.player, this.playerHealth, null, this);
+    //game.physics.arcade.collide(this.pAttack, this.enemies, this.enemyHealth, null, this);
 
 }
 
